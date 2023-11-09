82bb38f6d13206e7ea5889acdcde3ad8fbbfdd02
Hirayamanawari <jonalynmanguba@gmail.com>
2023-11-09 12:57:36 +0800

    navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,51 +25,51 @@ const Navbar = () => {
       </div>
 
       <div className="object-cover md:ml-10 md:mr-20">
-        <Link to="/Home">
+        <Link to="/">
           <img
             src={logo}
             alt="Logo"
-            className="hidden md:block lg:block md:text-2xl lg:text-2xl h-[80px] w-[100px]"
+            className=" md:block lg:block md:text-2xl lg:text-2xl h-[80px] w-[100px]"
           />
         </Link>
       </div>
 
-      <div className="md:mb-10 md:px-10 md:w-1/2 lg:w-[850px] xs:hidden">
+      <div className="md:mb-10 md:px-10 md:w-1/2 lg:w-[850px] xs:hidden mt-2 lg:mt-10 md:m-8">
         <div className="md:w-4/5">
           <div className="rounded-lg bg-textw">
             <form className="flex flex-row justify-between">
               <input
                 type="search"
                 placeholder="Sign up and get up to 100% discount on your first order"
-                className="text-sm h-10 lg:w-4/5 md:w-99 rounded-full lg:mr-1"
+                className="text-sm h-10 lg:w-4/5 md:w-99 rounded-full lg:mr-1 px-2"
               />
-              <span className="text-primary lg:w-5 mt-3">
-                <AiOutlineSearch />
+              <span className="text-primary lg:w-5 mt-3 px-2 lg:px-8">
+                < AiOutlineSearch />
               </span>
             </form>
           </div>
         </div>
       </div>
 
-      <div className="flex md:flex-wrap items-center md:mt-10 gap-1">
-        <ul className="flex flex-row gap-2 text-textw">
+      <div className="flex md:flex-wrap flex-col items-center md:mt-8 gap-1">
+        <ul className="flex gap-2 text-textw">
           <Link to="/Wishlist">
             <li className="md:block px-3 flex flex-col">
-              <span className=" md:block lg:block ">
+              <span className=" md:block lg:block text-2xl  ml-1 lg:ml-8">
                 <AiOutlineHeart />
               </span>
-              <span className='hidden md:block lg:block'>Wishlist</span>
+              <span className='hidden md:block lg:block text-xs  lg: ml-5 uppercase'>Wishlist</span>
             </li>
           </Link>
-          <li className="ml-0 text-textw">
+          <li className=" text-textw text-2xl mr-1 lg:mr-5">
             <AiOutlineShoppingCart />
-            <span className="hidden md:block lg:block">Cart</span>
+            <span className="hidden md:block lg:block text-xs uppercase">Cart</span>
           </li>
-          <li className="mr-2 text-textw">
-            <span className="text-2xl">
-              <AiOutlineUser />
+          <li className=" text-textw text-xs">
+            <span className="text-2xl ">
+           <Link to='/Login'>   <AiOutlineUser /></Link>
             </span>
-            <span className="hidden md:block lg:block text-xs">
+            <span className="hidden md:block lg:block text-xs ml-px	">
               <Link to ='/Login'>LOGIN</Link>
 
             </span>
